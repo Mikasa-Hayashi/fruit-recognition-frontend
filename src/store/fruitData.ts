79042db860a9d9f7c850e7a213db675791ffad0b
Ivan <ivandenisovich2005@gmail.com>
2026-04-20 11:16:14 +0300

    chore: add orange info

---
+++ src/store/fruitData.ts
@@ -0,0 +1,54 @@
+export const fruitData = [
+    {
+        id: 1,
+        slug: "orange",
+        scientific_name: "Citrus × sinensis",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk3b8Bvk13EwXdAMG51LCLiHG7IoAq4bzZBw&s",
+        name: "Апельсин",
+        description: "Сочный цитрусовый фрукт, богатый витамином C",
+        vitamins: [
+            { label: "Витамин A", value: "11 мкг" },
+            { label: "Витамин B1", value: "0.087 мг" },
+            { label: "Витамин B2", value: "0.04 мг" },
+            { label: "Витамин B3/PP", value: "0.282 мг" },
+            { label: "Витамин B5", value: "0.25 мг" },
+            { label: "Витамин B6", value: "0.06 мг" },
+            { label: "Витамин B7", value: "50 мкг" },
+            { label: "Витамин B9", value: "30 мкг" },
+            { label: "Витамин C", value: "53.2 мг" },
+            { label: "Витамин E", value: "0.18 мг" }
+        ],
+        macronutrients: [
+            { label: "Калий", value: "181 мг" },
+            { label: "Кальций", value: "40 мг" },
+            { label: "Магний", value: "10 мг" },
+            { label: "Натрий", value: "0 мг" },
+            { label: "Сера", value: "9.4 мг" },
+            { label: "Фосфор", value: "14 мг" },
+            { label: "Хлор", value: "3 мг" }
+        ],
+        micronutrients: [
+            { label: "Железо", value: "0.1 мг" },
+            { label: "Йод", value: "2 мкг" },
+            { label: "Кобальт", value: "0.1 мкг" },
+            { label: "Марганец", value: "0.03 мг" },
+            { label: "Медь", value: "0.04 мг" },
+            { label: "Молибден", value: "1 мкг" },
+            { label: "Селен", value: "0.5 мкг" },
+            { label: "Фтор", value: "17 мкг" },
+            { label: "Хром", value: "0.5 мкг" },
+            { label: "Цинк", value: "0.07 мг" }
+        ],
+        nutrition: [
+            { label: "Белки", value: "0.9 г" },
+            { label: "Жиры", value: "0.1 г" },
+            { label: "Углеводы", value: "11.8 г" },
+            { label: "Калорийность", value: "47 ккал" },
+            { label: "Пищевые волокна", value: "2.4 г" },
+            { label: "Вода", value: "86.8 г" }
+        ]
+    },
+    {
+
+    }
+]
